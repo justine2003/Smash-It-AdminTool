48a06f99cab808a5279f20c495cd7f1f368b90e3
justine2003 <justingilberto888@gmail.com>
2025-09-30 22:54:35 -0600

    Producto conectado a la base de datos

---
+++ Models/Rol.cs
@@ -0,0 +1,9 @@
+﻿namespace SGA_Smash.Models
+{
+    public class Rol
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+    }
+}
---
+++ Models/Usuario.cs
@@ -0,0 +1,14 @@
+﻿namespace SGA_Smash.Models
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Correo { get; set; }
+        public string Contrasena { get; set; }
+        public DateTime FechaCreacion { get; set; }
+        public DateTime? UltimoAcceso { get; set; }
+        public int RolId { get; set; }
+        public Rol? Rol { get; set; }
+    }
+}
